6b33a814a2639b615690504e731eed27c7c3d9a8
Victor Elliott <victor.elliott@tufts.edu>
2021-08-03 15:37:03 -0400

    Create wardrobe add view

--- rewear/urls.py
+++ rewear/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wardrobe/', include('wardrobe.urls'))
+    path('wardrobe/', include('wardrobe.urls')),
 ]
--- wardrobe/views.py
+++ wardrobe/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("wardrobe/index/")
+    return HttpResponse(
+        """Welcome to the index view. This is where the user will be able to
+        view, sort, filter, and search their list of clothes."""
+        )
+    
+def add(request):
+    return HttpResponse(
+        """Welcome to the add view. This is where new items of clothing 
+        will be added to the user's list."""
+        )
