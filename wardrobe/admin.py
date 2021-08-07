fc3831b010de9236216ff17837a4c27671a33659
Victor Elliott <victor.elliott@tufts.edu>
2021-08-07 16:46:29 -0400

    Create ClothingTag model

--- wardrobe/admin.py
+++ wardrobe/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import ClothingItem, WearDate
+from .models import ClothingItem, ClothingWearDate, ClothingTag
 
 # Register your models here.
 admin.site.register(ClothingItem)
-admin.site.register(WearDate)
+admin.site.register(ClothingWearDate)
+admin.site.register(ClothingTag)
--- wardrobe/models.py
+++ wardrobe/models.py
@@ -1,21 +1,55 @@
 from django.db import models
 
 # Create your models here.
+class ClothingTag(models.Model):
+    """Model representing a user-defined tag to be applied to clothing items.
+    Tags are grouped into categories such as colors, brands, clothing types,
+    etc."""
+    
+    BRAND = 'BR'
+    COLOR = 'CO'
+    TYPE = 'TY'
+    OTHER = 'OT'
+    
+    TAG_CATEGORY_CHOICES = [
+        (BRAND, 'Brand'),
+        (COLOR, 'Color'),
+        (TYPE, 'Type'),
+        (OTHER, 'Other'),
+    ]
+    
+    # REQUIRED FIELDS
+    name = models.CharField(max_length=200, unique=True)
+    category = models.CharField(max_length=200, choices=TAG_CATEGORY_CHOICES)
+    
+    class Meta:
+        ordering = ['category', 'name']
+        
+    def __str__(self):
+        """Returns a string for representing a ClothingTag object."""
+        return self.name
+        
+    def get_absolute_url(self):
+        pass
+
 class ClothingItem(models.Model):
     """Model representing an individual item of clothing."""
     
     # REQUIRED FIELDS
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200, unique=True)
     
     # OPTIONAL FIELDS
     picture = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=11, decimal_places=2, null=True, blank=True)
-    # Stores numbers with two decimal places up to one billion.
+    # Stores numbers with two decimal places up to one billion (allows negatives).
     purchase_date = models.DateField(null=True, blank=True)
     # If not provided, use the earliest wear date.
-    previous_wears = models.IntegerField(null=True, blank=True)
+    previous_wears = models.PositiveIntegerField(null=True, blank=True)
     # If provided, add to the number of wear dates.
     description = models.TextField(blank=True)
+    tags = models.ManyToManyField(ClothingTag)
+    # Belongs to ClothingItem because tags will be added to clothes in the
+    # form, not the other way around.
     
     class Meta:
         ordering = ['name']
@@ -27,7 +61,7 @@ class ClothingItem(models.Model):
     def get_absolute_url(self):
         pass
         
-class WearDate(models.Model):
+class ClothingWearDate(models.Model):
     """Model representing a date on which a clothing item was worn."""
     
     # REQUIRED FIELDS
@@ -40,7 +74,7 @@ class WearDate(models.Model):
         ordering = ['date']
     
     def __str__(self):
-        """Returns a string for representing a WearDate object."""
+        """Returns a string for representing a ClothingWearDate object."""
         return self.date.isoformat()
         
     def get_absolute_url(self):
