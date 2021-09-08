3831bb473517c575ef1e9e23422404caed2e0181
Victor Elliott <victor.elliott@tufts.edu>
2021-09-08 10:37:02 -0400

    Create templates

--- wardrobe/views.py
+++ wardrobe/views.py
@@ -3,29 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(
-        """Welcome to the index view. This is where the user will be able to
-        view, sort, filter, and search their list of clothes."""
-        )
+    return render(request, 'wardrobe/index.html')
     
 def add(request):
-    return HttpResponse(
-        """Welcome to the add view. This is where new items of clothing 
-        will be added to the user's list."""
-        )
+    return render(request, 'wardrobe/add.html')
 
 def log(request):
-    return HttpResponse(
-        """View for displaying the calendar and adding clothing items to dates."""
-    )
+    return render(request, 'wardrobe/log.html')
 
 def item(request, pk):
-    return HttpResponse(
-        "View for adding a new item to the wardrobe (" + str(pk) + ")."
-    )
+    return render(request, 'wardrobe/item.html')
 
 def edit(request, pk):
-    return HttpResponse(
-        "View for editing the details of a clothing item (" + str(pk) + ")."
-    )
-
+    return render(request, 'wardrobe/edit.html')
